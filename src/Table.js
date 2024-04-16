3a2dea3c4d0b36df2b585ed526fad50c23d5b7b8
sayed-soeb <sayed.soeb1@gmail.com>
2024-04-16 21:28:48 +0530

    design handled

--- src/Table.js
+++ src/Table.js
@@ -2,21 +2,23 @@ import React, { useState, useEffect } from 'react';
 import './style.css'; // Stylesheet for the table
 
 const dummyData = [
-    { id: 1, name: 'Indian Institute of Technology Bombay', ranking: 1, rating: 4.5, fees: 150000, userRating: 4.2, featured: true },
-    { id: 2, name: 'Delhi University', ranking: 2, rating: 4.3, fees: 50000, userRating: 4.0, featured: false },
-    { id: 3, name: 'Indian Institute of Management Ahmedabad', ranking: 3, rating: 4.7, fees: 200000, userRating: 4.6, featured: true },
-    { id: 4, name: 'Jawaharlal Nehru University', ranking: 4, rating: 4.0, fees: 30000, userRating: 3.8, featured: false },
-    { id: 5, name: 'Banaras Hindu University', ranking: 5, rating: 4.2, fees: 40000, userRating: 4.1, featured: false },
-    { id: 6, name: 'University of Mumbai', ranking: 6, rating: 4.1, fees: 60000, userRating: 3.9, featured: false },
-    { id: 7, name: 'Indian Institute of Technology Delhi', ranking: 7, rating: 4.6, fees: 180000, userRating: 4.4, featured: true },
-    { id: 8, name: 'University of Calcutta', ranking: 8, rating: 4.0, fees: 45000, userRating: 3.7, featured: false },
-    { id: 9, name: 'Anna University', ranking: 9, rating: 4.4, fees: 120000, userRating: 4.3, featured: true },
-    { id: 10, name: 'Indian Institute of Science Bangalore', ranking: 10, rating: 4.8, fees: 250000, userRating: 4.7, featured: true },
-    { id: 11, name: 'University of Delhi', ranking: 11, rating: 4.2, fees: 55000, userRating: 4.0, featured: false },
-    { id: 12, name: 'Indian Institute of Technology Madras', ranking: 12, rating: 4.6, fees: 160000, userRating: 4.5, featured: true },
-    { id: 13, name: 'University of Pune', ranking: 13, rating: 4.1, fees: 50000, userRating: 3.8, featured: false },
-    { id: 14, name: 'Indian Institute of Management Bangalore', ranking: 14, rating: 4.7, fees: 220000, userRating: 4.6, featured: true },
-    { id: 15, name: 'University of Hyderabad', ranking: 15, rating: 4.3, fees: 70000, userRating: 4.2, featured: false },
+    { id: 1, name: 'Indian Institute of Technology Bombay',
+    logo:"https://www.drupal.org/files/styles/grid-4-2x/public/iitblogo.png?itok=GvvDLvZf",ranking: 1, rating: 4.5, fees: 150000, userRating: 4.2, featured: true },
+    { id: 2, name: 'Delhi University',
+    logo:"https://seeklogo.com/images/U/university-of-delhi-logo-3DE170CB5E-seeklogo.com.png", ranking: 2, rating: 4.3, fees: 50000, userRating: 4.0, featured: false },
+    { id: 3, name: 'Indian Institute of Management Ahmedabad',logo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8DMZrWw2JZosNOuj4d6SLBIuyzewL9oMGEg&s", ranking: 3, rating: 4.7, fees: 200000, userRating: 4.6, featured: true },
+    { id: 4, name: 'Jawaharlal Nehru University',logo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVI4XJYGhTfXRl2G49O9AGAHV9E7Yu3lLzywNKQFE8OQ&s", ranking: 4, rating: 4.0, fees: 30000, userRating: 3.8, featured: false },
+    { id: 5, name: 'Banaras Hindu University',logo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU_dyOPcZq5ve-UoglgXRbg-4QGzII3Djkry9K5dPumg&s", ranking: 5, rating: 4.2, fees: 40000, userRating: 4.1, featured: false },
+    { id: 6, name: 'University of Mumbai',logo:"https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/University_of_Mumbai_coat_of_arms.svg/1200px-University_of_Mumbai_coat_of_arms.svg.png", ranking: 6, rating: 4.1, fees: 60000, userRating: 3.9, featured: false },
+    { id: 7, name: 'Indian Institute of Technology Delhi',logo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqzf9SyT0oZEYAT2LZo-nGm8b9fiT_n-ICnsWQuXdv2A&s", ranking: 7, rating: 4.6, fees: 180000, userRating: 4.4, featured: true },
+    { id: 8, name: 'University of Calcutta',logo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScP5cSa1jMlyHwPWcwGvM34WNyZTJrFC82SRKy-y7hXw&s", ranking: 8, rating: 4.0, fees: 45000, userRating: 3.7, featured: false },
+    { id: 9, name: 'Anna University',logo:"https://upload.wikimedia.org/wikipedia/en/thumb/4/49/Anna_University_Logo.svg/1200px-Anna_University_Logo.svg.png", ranking: 9, rating: 4.4, fees: 120000, userRating: 4.3, featured: true },
+    { id: 10, name: 'Indian Institute of Science Bangalore',logo:"https://i2.wp.com/cst.iisc.ac.in/sudesi/wp-content/uploads/2017/12/cropped-Indian_Institute_of_Science_logo.svg_.png?fit=512%2C512&ssl=1", ranking: 10, rating: 4.8, fees: 250000, userRating: 4.7, featured: true },
+    { id: 11, name: 'University of Delhi',logo:"https://upload.wikimedia.org/wikipedia/commons/6/62/Delhi_University%27s_official_logo.png", ranking: 11, rating: 4.2, fees: 55000, userRating: 4.0, featured: false },
+    { id: 12, name: 'Indian Institute of Technology Madras',logo:"https://w7.pngwing.com/pngs/54/748/png-transparent-indian-institute-of-technology-madras-department-of-management-studies-iit-madras-doctor-of-philosophy-student-indian-institutes-of-technology-student-people-logo-india.png", ranking: 12, rating: 4.6, fees: 160000, userRating: 4.5, featured: true },
+    { id: 13, name: 'University of Pune',logo:"https://w7.pngwing.com/pngs/251/546/png-transparent-department-of-physics-nashik-centre-for-modeling-and-simulation-university-college-schedule-miscellaneous-orange-logo.png", ranking: 13, rating: 4.1, fees: 50000, userRating: 3.8, featured: false },
+    { id: 14, name: 'Indian Institute of Management Bangalore',logo:"https://upload.wikimedia.org/wikipedia/en/a/a2/IIM_Bangalore_Logo.svg", ranking: 14, rating: 4.7, fees: 220000, userRating: 4.6, featured: true },
+    { id: 15, name: 'University of Hyderabad',logo:"https://upload.wikimedia.org/wikipedia/en/9/9e/University_of_Hyderabad_Logo.png", ranking: 15, rating: 4.3, fees: 70000, userRating: 4.2, featured: false },
   ];
 
 const Table = () => {
@@ -120,11 +122,19 @@ const Table = () => {
                     #{college.ranking}</td>
                 <td className={college.featured ? 'featured-name' : ''}>
                 {college.featured && (
-                  <div className="featured-label">Featured</div>
+                <>
+                <div className='feature-banner'>
+                <div className='outer'></div>
+                <div className="featured-label">Featured</div>
+                </div>
+                </>
                 )}
-                {college.name}</td>
+                <div style={{display:'flex',alignItems:'center',gap:'10px'}}>
+                <img style={{width:'40px',height:'40px'}} src={college.logo} alt='logo' />
+                {college.name}
+                </div></td>
                 <td>{college.rating}</td>
-                <td>{college.fees}<br />
+                <td>₹ {college.fees}<br />
                 <p className='fees'>BE/B.Tech<br />
                 -1st Year Fees</p></td>
                 <td>{college.userRating}/5<br />
